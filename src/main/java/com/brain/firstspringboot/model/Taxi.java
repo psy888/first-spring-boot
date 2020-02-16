61dc40fea7d7207483742043a2dac1d1d09894bf
psy888 <tbilisi.zp.snab@gmail.com>
2020-02-16 19:08:53 +0200

    realize get first free taxi

--- src/main/java/com/brain/firstspringboot/model/Taxi.java
+++ src/main/java/com/brain/firstspringboot/model/Taxi.java
@@ -1,10 +1,12 @@
 package com.brain.firstspringboot.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.DayOfWeek;
 
 @AllArgsConstructor
+@Data
 public class Taxi {
     private String car;
     private String carNumber;
--- src/main/java/com/brain/firstspringboot/repository/TaxiRepository.java
+++ src/main/java/com/brain/firstspringboot/repository/TaxiRepository.java
@@ -4,10 +4,19 @@ import com.brain.firstspringboot.model.Taxi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class TaxiRepository {
     @Autowired
     List<Taxi> taxiList;
+
+    public Taxi getFreeTaxi(){
+        return taxiList.stream()
+                .filter(taxi -> taxi.getRestDay()!= LocalDateTime.now().getDayOfWeek())
+                .findFirst()
+                .orElse(null);
+    }
 }
