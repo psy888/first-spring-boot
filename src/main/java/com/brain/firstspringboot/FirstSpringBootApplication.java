95570759e44f4fac2907fc4f247fbf39d061a258
psy888 <tbilisi.zp.snab@gmail.com>
2020-02-16 18:55:13 +0200

    refactor EventListener to commandLineRunner

--- src/main/java/com/brain/firstspringboot/FirstSpringBootApplication.java
+++ src/main/java/com/brain/firstspringboot/FirstSpringBootApplication.java
@@ -1,20 +1,35 @@
 package com.brain.firstspringboot;
 
+import com.brain.firstspringboot.component.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FirstSpringBootApplication {
 
+	@Autowired
+	private Person person;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBootApplication.class, args);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void onRun(){
-		System.out.println("System started");
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void onRun(){
+//		System.out.println("System started");
+//		person.goTaxi();
+//	}
+
+	@Bean
+	public CommandLineRunner run(){
+		return args -> {
+			person.goTaxi();
+		};
 	}
 
 }
--- src/main/java/com/brain/firstspringboot/component/Person.java
+++ src/main/java/com/brain/firstspringboot/component/Person.java
@@ -2,17 +2,20 @@ package com.brain.firstspringboot.component;
 
 import com.brain.firstspringboot.service.TaxiCaller;
 import lombok.Data;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@ToString
 public class Person {
     @Value("${user.properties.person.name}")
     private String name;
     @Value("${user.properties.person.phone}")
     private String phone;
-
+    @Autowired
     private TaxiCaller taxiCaller;
 
     public void goTaxi() {
