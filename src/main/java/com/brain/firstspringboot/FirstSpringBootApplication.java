0dbc29de57cbc9e8c8e2ace879e47685f75aa8c6
psy888 <tbilisi.zp.snab@gmail.com>
2020-02-16 16:51:16 +0200

    starting app config

--- src/main/java/com/brain/firstspringboot/FirstSpringBootApplication.java
+++ src/main/java/com/brain/firstspringboot/FirstSpringBootApplication.java
@@ -2,12 +2,19 @@ package com.brain.firstspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FirstSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void onRun(){
+		System.out.println("System started");
 	}
 
 }
